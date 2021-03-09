4b495b56b70a347b27d1e6fb7baa158a0af8b945
jeewon94 <jayyy1224@gmail.com>
2021-03-09 16:58:56 +0900

    03/09 (화) 풀페이지로 포트폴리오 프로젝트 시작

---
+++ js/typing.js
@@ -0,0 +1,17 @@
+// var myname = document.getElementById('myname');
+
+// var typewriter = new Typewriter(myname, {
+//     loop: true
+// });
+
+// typewriter.typeString('Welcome!')
+//     .pauseFor(2500)
+//     .deleteAll()
+//     .typeString("I'm jeewon")
+//     .pauseFor(2500)
+//     .deleteChars(7)
+//     .typeString('<strong>I am a publisher</strong>')
+//     .pauseFor(2500)
+//     .start();
+
+
